769c6cb506ad48d5831c209691a34a6741bc3393
yashu sthapit <yashusthapit@gmail.com>
2024-06-27 16:23:45 +0545

    feat: created the bottom navigation component in dashboard

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,12 +1,62 @@
 import React from "react";
-import Header from "../components/Header";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Text } from "react-native";
+import { BottomNavigation } from "react-native-paper";
+import { Container } from "../components";
+
+const Home = () => (
+  <SafeAreaView>
+    <Text>Welcome to Home</Text>
+  </SafeAreaView>
+);
+
+const Doctor = () => (
+  <SafeAreaView>
+    <Text>Welcome to Doctor Portal</Text>
+  </SafeAreaView>
+);
+
+const Setting = () => (
+  <SafeAreaView>
+    <Text>Settings Page</Text>
+  </SafeAreaView>
+);
 
 const DashboardScreen = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: "home",
+      title: "Home",
+      focusedIcon: "home",
+      unfocusedIcon: "home-outline",
+    },
+    {
+      key: "doctor",
+      title: "Doctor",
+      focusedIcon: "account",
+      unfocusedIcon: "account-outline",
+    },
+    {
+      key: "setting",
+      title: "Setting",
+      focusedIcon: "archive-settings",
+      unfocusedIcon: "archive-settings-outline",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: Home,
+    doctor: Doctor,
+    setting: Setting,
+  });
   return (
-    <SafeAreaView>
-      <Header>Welcome to Dashboard</Header>
-    </SafeAreaView>
+    <>
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
+    </>
   );
 };
 
