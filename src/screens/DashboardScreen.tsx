5e3f703178e0e58fda8a48ed6a88f8c86bd08699
yashu sthapit <yashusthapit@gmail.com>
2024-07-05 16:34:39 +0545

    feat: changed the tab navigation without using react native paper

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,153 +1,183 @@
-// Defaults
-import React from "react";
-import {
-  Alert,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Appbar, Avatar, BottomNavigation, Drawer } from "react-native-paper";
-
-// Components
-import { Container } from "../components";
+import { Appbar, Avatar, BottomNavigation } from "react-native-paper";
+import { useNavigationState, useIsFocused } from "@react-navigation/native";
 
 // Screens
 import HomeScreen from "./HomeScreen";
 import DoctorScreen from "./DoctorScreen";
-import HospitalScreen from "./HospitalScreen";
 import AppointmentScreen from "./AppointmentScreen";
+import HospitalStackScreen from "./HospitalStackScreen";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 
-const Setting = () => (
-  <SafeAreaView>
-    <Text>Settings Page</Text>
-  </SafeAreaView>
-);
+const Tab = createMaterialBottomTabNavigator();
 
-const HospitalStack = createStackNavigator();
+const DashboardScreen = ({ navigation }: any) => {
+  const [index, setIndex] = useState(0);
+  const [isNavBarVisible, setIsNavBarVisible] = useState(true);
 
-const HospitalStackScreen = ({ navigation }: any) => {
-  return (
-    <HospitalStack.Navigator
-      initialRouteName="HospitalScreen"
-      screenOptions={{
-        headerShown: false,
-        cardStyle: { backgroundColor: "white" },
-      }}
-    >
-      <HospitalStack.Screen name="Hospital" component={HospitalScreen} />
-      <HospitalStack.Screen
-        name="DoctorScreen"
-        component={DoctorScreen}
-        options={{
-          headerShown: true,
-          headerTitle: "Doctor",
-          headerTintColor: "white",
-          headerStyle: {
-            backgroundColor: "#01B9EB",
-          },
-        }}
-      />
-      <HospitalStack.Screen
-        name="AppointmentScreen"
-        component={AppointmentScreen}
-        options={{
-          headerShown: true,
-          headerTitle: "Book an appointment",
-          headerTintColor: "white",
-          headerStyle: {
-            backgroundColor: "#01B9EB",
-          },
-        }}
-      />
-    </HospitalStack.Navigator>
+  // const routes = [
+  //   {
+  //     key: "home",
+  //     title: "Home",
+  //     focusedIcon: "home",
+  //     unfocusedIcon: "home-outline",
+  //   },
+  //   {
+  //     key: "doctor",
+  //     title: "Doctor",
+  //     focusedIcon: "account",
+  //     unfocusedIcon: "account-outline",
+  //   },
+  //   {
+  //     key: "hospital",
+  //     title: "Hospital",
+  //     focusedIcon: "home-city",
+  //     unfocusedIcon: "home-city-outline",
+  //   },
+  //   {
+  //     key: "appointment",
+  //     title: "Appointment",
+  //     focusedIcon: "calendar-check",
+  //     unfocusedIcon: "calendar-blank",
+  //   },
+  // ];
+
+  // const renderScene = BottomNavigation.SceneMap({
+  //   home: HomeScreen,
+  //   doctor: DoctorScreen,
+  //   hospital: HospitalStackScreen,
+  //   appointment: AppointmentScreen,
+  // });
+
+  // const currentTitle = routes[index].title;
+
+  // Get the current navigation state
+  const navigationState: any = useNavigationState((state) => state);
+
+  const getCurrentRouteName: any = (state: any) => {
+    const route = state.routes[state.index];
+
+    if (route.state) {
+      return getCurrentRouteName(route.state);
+    }
+    return route.name;
+  };
+
+  console.log(
+    "getCurrentRouteName",
+    navigationState?.routes[navigationState?.routes?.length - 1]?.state?.routes
   );
-};
+  const currentRouteName = getCurrentRouteName(navigationState);
 
-const DashboardScreen = ({ navigation, route }: any) => {
-  const [index, setIndex] = React.useState(0);
+  useEffect(() => {
+    const shouldHideNavBar =
+      currentRouteName === "DoctorScreen" ||
+      currentRouteName === "AppointmentScreen";
+    setIsNavBarVisible(!shouldHideNavBar);
+  }, [currentRouteName]);
 
-  const [routes] = React.useState([
-    {
-      key: "home",
-      title: "Home",
-      focusedIcon: "home",
-      unfocusedIcon: "home-outline",
-    },
-    {
-      key: "doctor",
-      title: "Doctor",
-      focusedIcon: "account",
-      unfocusedIcon: "account-outline",
-    },
-    {
-      key: "hospital",
-      title: "Hospital",
-      focusedIcon: "home-city",
-      unfocusedIcon: "home-city-outline",
-    },
-    {
-      key: "appointment",
-      title: "Appointment",
-      focusedIcon: "calendar-check",
-      unfocusedIcon: "calendar-blank",
-    },
-  ]);
-
-  const renderScene = BottomNavigation.SceneMap({
-    home: HomeScreen,
-    doctor: DoctorScreen,
-    hospital: HospitalStackScreen,
-    appointment: AppointmentScreen,
-  });
+  useEffect(() => {
+    navigation.getParent()?.setOptions({
+      tabBarStyle: {
+        display: "none",
+      },
+    });
+    return () =>
+      navigation.getParent()?.setOptions({
+        tabBarStyle: undefined,
+      });
+  }, [navigation]);
 
   return (
     <>
-      {/* <Appbar.Header className="px-6 bg-[#01B9EB] flex-row items-center justify-between">
-        <View className="flex-col items-start">
-          <Text className="text-xl font-bold text-white">Hi, Yashu</Text>
-          <Text className="text-xs text-white">How are you?</Text>
-        </View>
-        <TouchableOpacity onPress={handleProfile}>
-          <Avatar.Image
-            source={{
-              uri: "https://media.licdn.com/dms/image/D4D03AQEzximBza-Klw/profile-displayphoto-shrink_200_200/0/1718525400486?e=2147483647&v=beta&t=h_Tk6sQ-1G-4XzV6VeyG6X5LQslqWLq-ShnoKaLNaoE",
-            }}
-            size={40}
-            // icon="account"
-            className="ml-auto bg-white text-black"
-          />
-        </TouchableOpacity>
-      </Appbar.Header> */}
-
-      {index !== 0 ? (
-        <></>
-      ) : (
-        <Appbar.Header className="px-6 bg-[#01B9EB] flex-row items-center justify-between">
-          <>
-            <View className="flex-col items-start">
-              <Text className="text-xl font-bold text-white">Hi, Yashu</Text>
-              <Text className="text-xs text-white">How are you?</Text>
-            </View>
-            <TouchableOpacity onPress={() => alert("Profile Clicked")}>
-              <Avatar.Image
-                source={{
-                  uri: "https://media.licdn.com/dms/image/D4D03AQEzximBza-Klw/profile-displayphoto-shrink_200_200/0/1718525400486?e=2147483647&v=beta&t=h_Tk6sQ-1G-4XzV6VeyG6X5LQslqWLq-ShnoKaLNaoE",
-                }}
-                size={40}
-                className="ml-auto bg-white text-black"
-              />
-            </TouchableOpacity>
-          </>
-        </Appbar.Header>
+      {isNavBarVisible && (
+        <>
+          {index !== 0 ? (
+            <Appbar.Header className="px-6 bg-[#01B9EB] flex-row items-center justify-between">
+              <View className="flex-col items-start">
+                <Text className="text-xl font-bold text-white">
+                  {/* {currentTitle} */}
+                </Text>
+              </View>
+            </Appbar.Header>
+          ) : (
+            <Appbar.Header className="px-6 bg-[#01B9EB] flex-row items-center justify-between">
+              <View className="flex-col items-start">
+                <Text className="text-xl font-bold text-white">Hi, Yashu</Text>
+                <Text className="text-xs text-white">How are you?</Text>
+              </View>
+              <TouchableOpacity onPress={() => alert("Profile Clicked")}>
+                <Avatar.Image
+                  source={{
+                    uri: "https://media.licdn.com/dms/image/D4D03AQEzximBza-Klw/profile-displayphoto-shrink_200_200/0/1718525400486?e=2147483647&v=beta&t=h_Tk6sQ-1G-4XzV6VeyG6X5LQslqWLq-ShnoKaLNaoE",
+                  }}
+                  size={40}
+                  className="ml-auto bg-white text-black"
+                />
+              </TouchableOpacity>
+            </Appbar.Header>
+          )}
+        </>
       )}
 
-      <BottomNavigation
+      {/* <BottomNavigation
         navigationState={{ index, routes }}
         onIndexChange={setIndex}
         renderScene={renderScene}
-      />
+        shifting={false}
+        
+      /> */}
+
+      <Tab.Navigator
+        activeColor="black"
+        inactiveColor="gray"
+        barStyle={{ backgroundColor: "white" }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Avatar.Icon
+                icon="home"
+                size={30}
+                color={color}
+                className="bg-transparent"
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Doctor"
+          component={DoctorScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Avatar.Icon
+                icon="doctor"
+                size={30}
+                color={color}
+                className="bg-transparent"
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Hospital"
+          component={HospitalStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Avatar.Icon
+                icon="hospital-building"
+                size={30}
+                color={color}
+                className="bg-transparent"
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </>
   );
 };
--- src/screens/HospitalStackScreen.tsx
+++ src/screens/HospitalStackScreen.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import AppointmentScreen from "./AppointmentScreen";
+import DoctorScreen from "./DoctorScreen";
+import HospitalScreen from "./HospitalScreen";
+
+const HospitalStack = createStackNavigator();
 
 const HospitalStackScreen = () => {
-  return <div></div>;
+  return (
+    <HospitalStack.Navigator
+      initialRouteName="HospitalScreen"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: "white" },
+      }}
+    >
+      <HospitalStack.Screen name="Hospital" component={HospitalScreen} />
+      <HospitalStack.Screen
+        name="DoctorScreen"
+        component={DoctorScreen}
+        options={{
+          headerShown: true,
+          headerTitle: "Doctor",
+          headerTintColor: "white",
+          headerStyle: {
+            backgroundColor: "#01B9EB",
+          },
+        }}
+      />
+      <HospitalStack.Screen
+        name="AppointmentScreen"
+        component={AppointmentScreen}
+        options={{
+          headerShown: true,
+          headerTitle: "Book an appointment",
+          headerTintColor: "white",
+          headerStyle: {
+            backgroundColor: "#01B9EB",
+          },
+        }}
+      />
+    </HospitalStack.Navigator>
+  );
 };
 
 export default HospitalStackScreen;
