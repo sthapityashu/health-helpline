0a5700912833deff9a12672f31db5c1774427dff
yashu sthapit <yashusthapit@gmail.com>
2024-07-31 11:11:53 +0545

    feat: changed the homescreen design

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -14,15 +14,17 @@ import { Container } from "../components";
 
 // Hooks
 import usePlatform from "../hooks/usePlatform";
+import BloodTestScreen from "./BloodTestScreen";
+import { BloodTest } from "@utils/data/constants/blood-test";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }: any) => {
   // Custom Hooks
   const { isIOS } = usePlatform();
 
   return (
     <Container>
       <ScrollView showsVerticalScrollIndicator={false}>
-        {/* Card Sction */}
+        {/* Card Section */}
         <View
           className={`w-full rounded-lg overflow-hidden ${
             isIOS ? "my-4" : "mb-4"
@@ -53,118 +55,198 @@ const HomeScreen = () => {
           </ImageBackground>
         </View>
 
-        {/* Doctor Speciality */}
-        <View className="flex flex-row items-center justify-between">
-          <Text className="text-xl font-bold">Doctor Speciality</Text>
-          <Text>See All</Text>
-        </View>
-
-        {/* Speciality Section */}
-        <View className="flex flex-row justify-between items-center my-2">
-          <View className="flex items-center justify-center gap-2">
-            <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
-              <Image
-                className="h-[38px] w-[38px]"
-                source={{
-                  uri: "https://cdn-icons-png.flaticon.com/512/7403/7403331.png",
-                }}
-              />
-            </View>
-            <Text>General</Text>
-          </View>
-
-          <View className="flex items-center justify-center gap-2">
-            <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
-              <Image
-                className="h-[38px] w-[38px]"
-                source={{
-                  uri: "https://cdn-icons-png.freepik.com/512/9445/9445780.png",
-                }}
-              />
-            </View>
-            <Text>Neurologic</Text>
-          </View>
-
-          <View className="flex items-center justify-center gap-2">
-            <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
-              <Image
-                className="h-[38px] w-[38px]"
-                source={{
-                  uri: "https://cdn-icons-png.flaticon.com/512/4228/4228684.png",
-                }}
-              />
-            </View>
-            <Text>Radiology</Text>
-          </View>
-
-          <View className="flex items-center justify-center gap-2">
-            <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
-              <Image
-                className="h-[38px] w-[38px]"
-                source={{
-                  uri: "https://cdn-icons-png.flaticon.com/512/10154/10154448.png",
-                }}
-              />
-            </View>
-            <Text>Pediatric</Text>
-          </View>
-        </View>
-
-        {/* Recomendation */}
-        <View className="flex flex-row items-center justify-between my-2">
-          <Text className="text-xl font-bold">Recommendation Doctor</Text>
-          <Text>See All</Text>
-        </View>
-
-        {/* Recomendation Card */}
         <>
-          <View className="bg-gray-100 w-full h-36 rounded-md my-2">
-            <View className="flex flex-row w-full h-full items-center justify-around py-3">
-              <Image
-                className="h-full w-32 rounded-md"
-                source={{
-                  uri: "https://thumbs.dreamstime.com/b/young-smiling-old-man-doctor-medical-specialist-medicine-concept-cute-d-icon-people-character-illustration-cartoon-minimal-young-279139332.jpg",
-                }}
-              />
-              <View>
-                <Text className="text-xl font-bold">Dr. Yashu Sthapit</Text>
-                <Text>Neuro Surgeon</Text>
-                <View className="flex flex-row items-center justify-start bg-gray-200 rounded-md my-2">
-                  <Avatar.Icon
-                    icon="star"
-                    color="#FFD700"
-                    className="bg-transparent px-0"
-                    size={32}
-                  />
-                  <Text>4.8 (4,278 reviews)</Text>
-                </View>
-              </View>
-            </View>
+          {/* Doctor Speciality */}
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-bold">Doctor Speciality</Text>
+            <Text>See All</Text>
           </View>
 
-          <View className="bg-gray-100 w-full h-36 rounded-md my-2">
-            <View className="flex flex-row w-full h-full items-center justify-around py-3">
-              <Image
-                className="h-full w-32 rounded-md"
-                source={{
-                  uri: "https://i.pinimg.com/736x/41/74/fa/4174fad3ab126ebcb3d977795400c854.jpg",
-                }}
-              />
-              <View>
-                <Text className="text-xl font-bold">Dr. Udip Yakha Rai</Text>
-                <Text>Radiologis</Text>
-                <View className="flex flex-row items-center justify-start bg-gray-200 rounded-md my-2">
-                  <Avatar.Icon
-                    icon="star"
-                    color="#FFD700"
-                    className="bg-transparent px-0"
-                    size={32}
-                  />
-                  <Text>4.8 (4,278 reviews)</Text>
-                </View>
+          {/* Speciality Section */}
+          <View className="flex flex-row justify-between items-center my-2">
+            <View className="flex items-center justify-center gap-2">
+              <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
+                <Image
+                  className="h-[38px] w-[38px]"
+                  source={{
+                    uri: "https://cdn-icons-png.flaticon.com/512/7403/7403331.png",
+                  }}
+                />
               </View>
+              <Text>General</Text>
+            </View>
+
+            <View className="flex items-center justify-center gap-2">
+              <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
+                <Image
+                  className="h-[38px] w-[38px]"
+                  source={{
+                    uri: "https://cdn-icons-png.freepik.com/512/9445/9445780.png",
+                  }}
+                />
+              </View>
+              <Text>Neurologic</Text>
+            </View>
+
+            <View className="flex items-center justify-center gap-2">
+              <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
+                <Image
+                  className="h-[38px] w-[38px]"
+                  source={{
+                    uri: "https://cdn-icons-png.flaticon.com/512/4228/4228684.png",
+                  }}
+                />
+              </View>
+              <Text>Radiology</Text>
+            </View>
+
+            <View className="flex items-center justify-center gap-2">
+              <View className="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center">
+                <Image
+                  className="h-[38px] w-[38px]"
+                  source={{
+                    uri: "https://cdn-icons-png.flaticon.com/512/10154/10154448.png",
+                  }}
+                />
+              </View>
+              <Text>Pediatric</Text>
             </View>
           </View>
+        </>
+
+        <>
+          {/* Recomendation */}
+          <View className="flex flex-row items-center justify-between my-2">
+            <Text className="text-xl font-bold">Recommended Doctor</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("DoctorScreen")}
+            >
+              <Text>See All</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Doctor */}
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            className="h-[100px]"
+          >
+            {/* Speciality Section */}
+            <View className="flex flex-row justify-between items-center my-2 gap-3 mt-1">
+              <TouchableOpacity
+                onPress={() => navigation.navigate("AppointmentScreen")}
+              >
+                <View className="flex items-center justify-center  p-2 rounded-lg w-[100px] h-full">
+                  <View className="w-16 h-16 flex items-center justify-center bg-green-300 rounded-full">
+                    <Image
+                      className="h-[50px] w-[50px]"
+                      source={{
+                        uri: "https://png.pngtree.com/png-clipart/20220911/original/pngtree-male-doctor-avatar-icon-illustration-png-image_8537702.png",
+                      }}
+                    />
+                  </View>
+                  <Text className="h-10 text-center pt-1">
+                    Dr. Yashu Sthapit
+                  </Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => navigation.navigate("AppointmentScreen")}
+              >
+                <View className="flex items-center justify-center  p-2 rounded-lg w-[100px] h-full">
+                  <View className="w-16 h-16 flex items-center justify-center bg-blue-300 rounded-full">
+                    <Image
+                      className="h-[50px] w-[50px]"
+                      source={{
+                        uri: "https://png.pngtree.com/png-clipart/20220911/original/pngtree-male-doctor-avatar-icon-illustration-png-image_8537702.png",
+                      }}
+                    />
+                  </View>
+                  <Text className="h-10 text-center pt-1">Dr. Udip Rai</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("AppointmentScreen")}
+              >
+                <View className="flex items-center justify-center  p-2 rounded-lg w-[100px] h-full">
+                  <View className="w-16 h-16 flex items-center justify-center bg-slate-300 rounded-full">
+                    <Image
+                      className="h-[50px] w-[50px]"
+                      source={{
+                        uri: "https://png.pngtree.com/png-clipart/20220911/original/pngtree-male-doctor-avatar-icon-illustration-png-image_8537702.png",
+                      }}
+                    />
+                  </View>
+                  <Text className="h-10 text-center pt-1">
+                    Dr. Bikesh Maharjan
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("AppointmentScreen")}
+              >
+                <View className="flex items-center justify-center  p-2 rounded-lg w-[100px] h-full">
+                  <View className="w-16 h-16 flex items-center justify-center bg-red-300 rounded-full">
+                    <Image
+                      className="h-[50px] w-[50px]"
+                      source={{
+                        uri: "https://png.pngtree.com/png-clipart/20220911/original/pngtree-male-doctor-avatar-icon-illustration-png-image_8537702.png",
+                      }}
+                    />
+                  </View>
+                  <Text className="h-10 text-center pt-1">
+                    Dr. Santosh Thapa
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("AppointmentScreen")}
+              >
+                <View className="flex items-center justify-center  p-2 rounded-lg w-[100px] h-full">
+                  <View className="w-16 h-16 flex items-center justify-center bg-orange-300 rounded-full">
+                    <Image
+                      className="h-[50px] w-[50px]"
+                      source={{
+                        uri: "https://png.pngtree.com/png-clipart/20220911/original/pngtree-male-doctor-avatar-icon-illustration-png-image_8537702.png",
+                      }}
+                    />
+                  </View>
+                  <Text className="h-10 text-center pt-1">
+                    Dr. Ram Maharjan
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </>
+
+        <>
+          <View className="flex flex-row items-center justify-between my-2">
+            <Text className="text-xl font-bold">Blood Test</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("BloodTestScreen")}
+            >
+              <Text>See All</Text>
+            </TouchableOpacity>
+          </View>
+          <>
+            {BloodTest.slice(0, 3).map((item: any, id: number) => (
+              <View className="bg-gray-100 w-full h-36 rounded-md my-2">
+                <View className="flex flex-row w-full h-full items-center justify-around">
+                  <Image
+                    className="h-full w-[40%] rounded-md"
+                    source={{
+                      uri: item.image,
+                    }}
+                  />
+                  <View className="w-[60%] px-4">
+                    <Text className="text-xl font-bold">{item.category}</Text>
+                  </View>
+                </View>
+              </View>
+            ))}
+          </>
         </>
       </ScrollView>
     </Container>
